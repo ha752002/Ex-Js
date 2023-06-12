b21614a1cf916b36138bb33872fed886fb7ed279
ha752002 <honggha75@gmail.com>
2023-06-12 22:06:23 +0700

    1.4

---
+++ src/level1/1.4/1-4.js
@@ -0,0 +1,15 @@
+
+/**
+ * Write a program that takes a list of numbers as input and displays the largest number in the list.
+ * @param {arr} array 
+ * @returns displays the largest number in the list
+ */
+function maxNumber(arr) {
+    if (Array.isArray(arr) && arr.length > 0) {
+        return Math.max(...arr);
+    } else {
+        return "Invalid input";
+    }
+}
+
+module.exports = maxNumber;
---
+++ src/level1/1.4/1-4.test.js
@@ -0,0 +1,14 @@
+const maxNumber = require("./1-4.js");
+
+test("largest number in arr", () => {
+    expect(maxNumber(["a", "b", "c"])).toBe("Invalid input");
+});
+
+test("largest number in arr", () => {
+    expect(maxNumber([5, 8, 1, 2, 3])).toBe(8);
+});
+
+test("largest number in arr", () => {
+    expect(maxNumber([])).toBe("Invalid input");
+});
+
